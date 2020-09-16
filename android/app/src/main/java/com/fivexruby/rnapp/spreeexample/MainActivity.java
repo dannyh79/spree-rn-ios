ee8d3578bd3959d4ed7281bd87803c2bfd6350e7
PastLeo <chgu82837@gmail.com>
2020-09-16 15:02:13 +0800

    run on android

--- android/app/src/main/java/com/fivexruby/rnapp/spreeexample/MainActivity.java
+++ android/app/src/main/java/com/fivexruby/rnapp/spreeexample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.spreeexamplernapp;
+package com.fivexruby.rnapp.spreeexample;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/fivexruby/rnapp/spreeexample/MainApplication.java
+++ android/app/src/main/java/com/fivexruby/rnapp/spreeexample/MainApplication.java
@@ -1,4 +1,4 @@
-package com.spreeexamplernapp;
+package com.fivexruby.rnapp.spreeexample;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.spreeexamplernapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.fivexruby.rnapp.spreeexample.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
