3535532c3ddda9bde5e49869f07c981a79395fae
PastLeo <chgu82837@gmail.com>
2020-09-23 13:37:30 +0800

    add logo

--- App.js
+++ App.js
@@ -14,17 +14,48 @@ import {
   View,
   Text,
   StatusBar,
+  ImageBackground,
 } from 'react-native';
 
 import {
-  Header,
   LearnMoreLinks,
   Colors,
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const Header = () => (
+  <ImageBackground
+    accessibilityRole={'image'}
+    source={require('./assets/logo.jpg')}
+    style={headerStyles.background}
+    imageStyle={headerStyles.logo}>
+    <Text style={headerStyles.text}>SpreeExample</Text>
+  </ImageBackground>
+);
+
+const headerStyles = StyleSheet.create({
+  background: {
+    paddingBottom: 40,
+    paddingTop: 96,
+    paddingHorizontal: 32,
+    backgroundColor: Colors.lighter,
+  },
+  logo: {
+    opacity: 0.15,
+    overflow: 'visible',
+    resizeMode: 'cover',
+    marginLeft: -96,
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: Colors.black,
+  },
+});
+
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
